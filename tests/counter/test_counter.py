94f0c20769e8addec8d166c743be8c18a9c226ab
BradockBr <robtognetti@gmail.com>
2023-04-28 17:15:23 -0300

    req 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences('data/jobs.csv', 'python') == 1639
+    assert count_ocurrences('data/jobs.csv', 'PyThoN') == 1639
+    assert count_ocurrences('data/jobs.csv', 'PYTHON') == 1639
+    assert count_ocurrences("data/jobs.csv", "Javascript") == 122
+    assert count_ocurrences("data/jobs.csv", "javascript") == 122
+    assert count_ocurrences("data/jobs.csv", "JAVASCRIPT") == 122
